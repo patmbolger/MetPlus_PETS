63c4d73da209d64dee61017ff26c61f243dc0eb5
Patrick Bolger <patmbolger@gmail.com>
2016-10-26 07:24:52 -0400

    enhanced resume DL tests

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -1,82 +1,81 @@
 class JobApplicationsController < ApplicationController
-	include JobApplicationsViewer
+  include JobApplicationsViewer
 
-	before_action :find_application
+  before_action :find_application
 
-	def accept
-		unless @job_application.active?
-			flash[:alert] = "Invalid action on inactive job application."
-		else
-			@job_application.accept
-			Event.create(:APP_ACCEPTED, @job_application) if @job_application.job_seeker.job_developer
-			flash[:info] = "Job application accepted."
-		end
-		redirect_to applications_job_url(@job_application.job)
-	end
+  def accept
+    unless @job_application.active?
+      flash[:alert] = "Invalid action on inactive job application."
+    else
+      @job_application.accept
+      Event.create(:APP_ACCEPTED, @job_application) if @job_application.job_seeker.job_developer
+      flash[:info] = "Job application accepted."
+    end
+    redirect_to applications_job_url(@job_application.job)
+  end
 
-	def reject
-		unless @job_application.active?
-			if request.xhr?
-				render json: {message: "Cannot reject an inactive job application",
-											status: 405}
-			else
-				flash[:alert] = "Cannot reject an inactive job application."
-				redirect_to applications_path(@job_application)
-			end
-		else
-			@job_application.reason_for_rejection = params[:reason_for_rejection]
-			@job_application.save
-			@job_application.reject
-			Event.create(:APP_REJECTED, @job_application) if @job_application.job_seeker.job_developer
+  def reject
+    unless @job_application.active?
+      if request.xhr?
+        render json: {message: "Cannot reject an inactive job application",
+                      status: 405}
+      else
+        flash[:alert] = "Cannot reject an inactive job application."
+        redirect_to applications_path(@job_application)
+      end
+    else
+      @job_application.reason_for_rejection = params[:reason_for_rejection]
+      @job_application.save
+      @job_application.reject
+      Event.create(:APP_REJECTED, @job_application) if @job_application.job_seeker.job_developer
 
-			if request.xhr?
-				render json: {message: "Job application rejected", status: 200}
-			else
-				flash[:notice] = "Job application rejected."
-				redirect_to controller: 'jobs', action: 'applications',
-										id: @job_application.job.id
-			end
-		end
-	end
+      if request.xhr?
+        render json: {message: "Job application rejected", status: 200}
+      else
+        flash[:notice] = "Job application rejected."
+        redirect_to controller: 'jobs', action: 'applications',
+                    id: @job_application.job.id
+      end
+    end
+  end
 
-	def show
-	end
+  def show
+  end
 
-	def find_application
-		begin
-			@job_application = JobApplication.find(params[:id])
-		rescue
-			flash[:alert] = "Job Application Entry not found."
-			redirect_back_or_default
-		end
-	end
+  def find_application
+    begin
+      @job_application = JobApplication.find(params[:id])
+    rescue
+      flash[:alert] = "Job Application Entry not found."
+      redirect_back_or_default
+    end
+  end
 
-	def download_resume
-		begin
-      # Don't need the following line because of the before_action method
-			job_application = JobApplication.find(params[:id])
+  def download_resume
+    begin
+      # The before_action method fetches the @job_application
 
-      # Without the prior line, this next line would be:
-      #     job_seeker = @job_application.job_seeker
-			job_seeker = job_application.job_seeker
+      job_seeker = @job_application.job_seeker
 
       raise RuntimeError, 'Resume not found in DB' if job_seeker.resumes.empty?
-			resume = job_seeker.resumes[0]
+      resume = job_seeker.resumes[0]
 
-			resume_file = ResumeCruncher.download_resume(resume.id)
+      resume_file = ResumeCruncher.download_resume(resume.id)
       raise RuntimeError, 'Resume not found in Cruncher' if resume_file.nil?
 
-			send_data resume_file.open.read, filename: resume.file_name
+      send_data resume_file.open.read, filename: resume.file_name
 
-		rescue RuntimeError => e
-			flash[:alert] = "Error: #{e}"
-			redirect_back_or_default
+    rescue RuntimeError => e
+      flash[:alert] = "Error: #{e}"
+      redirect_back_or_default
 
-		ensure
-			resume_file.close
-			resume_file.unlink
-		end
+    ensure
+      if resume_file
+        resume_file.close
+        resume_file.unlink
+      end
+    end
 
-	end
+  end
 
 end
